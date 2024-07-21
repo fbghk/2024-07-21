a024f7176368d312be0ac144cf45cb3724675f52
fbghk <im706374@gmail.com>
2024-07-22 00:44:43 +0900

    prac | 예제 05-11

--- index.js
+++ index.js
@@ -1,15 +1,2 @@
-function foo () {
-  return
-  {}
-  // asi => return; {};
-  // 개발자 => return{};
-}
-
-console.log(foo());
-
-var bar = function () {}
-(function() {})();
-
-//asi 예측 -> var bar = function () {} (function() {})();
-//개발자 예측 -> var bar = function () {}; (function() {})();
-//* Type Error: (intermediate value)(...) is not a function
+var x;
+x = 1+2;
