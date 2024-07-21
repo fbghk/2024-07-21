9d613908844c24152c0ef404890dc68a588602bc
fbghk <im706374@gmail.com>
2024-07-22 00:58:19 +0900

    prac | 예제 06-03

--- index.js
+++ index.js
@@ -1,9 +1,4 @@
-var binary = 0b01000001;
-var octal = 0o101;
-var hex = 0x41;
+console.log(1 === 1.0); //true
 
-console.log(binary); //65
-console.log(octal); //65
-console.log(hex); //65
-console.log(binary === octal); //true
-console.log(octal === hex); //true
+console.log(4 / 2); //2
+console.log(3 / 2); //1.5
