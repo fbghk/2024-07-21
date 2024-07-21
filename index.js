8b8e860c81210be23b7472c5fe55beedac56205c
fbghk <im706374@gmail.com>
2024-07-21 14:17:23 +0900

    prac | 예제 04-02

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
-const arr = [1, 2, 3];
-arr.forEach(alert);
+var userId = 1;
+var userName  'Lee';
+
+var user = {id : 1, name: 'Lee'};
+
+var users = [
+  { id: 1, name: 'Lee'},
+  { id: 2, name: 'Kim'}
+]
