5d4a027156e15a798e277a74756633a045b6582c
fbghk <im706374@gmail.com>
2024-07-22 01:01:55 +0900

    prac | 예제 06-04

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-console.log(1 === 1.0); //true
+console.log(10 / 0); //Infinity
 
-console.log(4 / 2); //2
-console.log(3 / 2); //1.5
+console.log(10 / -0); // -Infinity
+console.log(1 * 'string'); // NaN
