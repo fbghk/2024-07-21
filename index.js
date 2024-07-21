1ce92cfaea77a3cbd46ca0fe4a4c37b69d28f7c5
fbghk <im706374@gmail.com>
2024-07-22 00:56:14 +0900

    prac | 예제 06-02

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
-var integer = 10;
-var double = 10.12;
-var negative = -20;
+var binary = 0b01000001;
+var octal = 0o101;
+var hex = 0x41;
+
+console.log(binary); //65
+console.log(octal); //65
+console.log(hex); //65
+console.log(binary === octal); //true
+console.log(octal === hex); //true
