e1c48bd062aa10d5e79d6d316105c56a65f579a5
fbghk <im706374@gmail.com>
2024-07-22 00:11:30 +0900

    prac | 예제 05-01

--- index.js
+++ index.js
@@ -1,9 +1,1 @@
-var firstName;
-
-var first_name;
-
-var FirstName;
-
-var strFirstName;
-var $elem = document.getElementById('myId');
-var observable$ = fromEvent(document, 'click');
+var sum = 10 + 20;
