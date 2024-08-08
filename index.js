b285a1d7d442445048cf69fbe04c2f09505c947c
fbghk <im706374@gmail.com>
2024-08-09 05:15:56 +0900

    prac | 예제 07-04

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
-var x = 1;
+var x = 5, relsult
 
-// ++ 연산자는 피연산자의 값을 변경하는 암묵적 할당이 이뤄진다.
-x++; // x = x + 1;
-console.log(x); //2
+//선할당 후증가(postfix increment operator)
+result = x++;
+console.log(result, x);// 5 6
 
-// --연산자는 피연산자의 값을 변경하는 암묵적 할당이 이뤄진다.
-x--; // x = x - 1;
-console.log(); // 1
+//선증가 후할당(prefix increment operator)
+result = ++x;
+console.log(result, x);// 7 7
+
+//선할당 후감소(postfix decrement operator)
+result = x--;
+console.log(result, x);// 7 6
+
+//선감소 후할당(prefix decrement operator)
+result = --x;
+console.log(result, x);// 5 5
