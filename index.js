f7bd5345ed651cf163abab8c3e5a2150e4005837
fbghk <im706374@gmail.com>
2024-08-18 17:15:37 +0900

    prac | 예제 07-05

--- index.js
+++ index.js
@@ -1,17 +1,3 @@
-var x = 5, relsult
-
-//선할당 후증가(postfix increment operator)
-result = x++;
-console.log(result, x);// 5 6
-
-//선증가 후할당(prefix increment operator)
-result = ++x;
-console.log(result, x);// 7 7
-
-//선할당 후감소(postfix decrement operator)
-result = x--;
-console.log(result, x);// 7 6
-
-//선감소 후할당(prefix decrement operator)
-result = --x;
-console.log(result, x);// 5 5
+// 아무런 효과가 없다.
++10; // -> 10
++(-10); // -> -10
