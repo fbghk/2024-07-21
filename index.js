073cf527d27cf3a575594d8c02724aa435e67c56
fbghk <im706374@gmail.com>
2024-08-18 18:23:49 +0900

    prac | 예제 07-13

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
-// 동등 비교
-5 == 5; // -> true
-
-//타입은 다르지만 암묵적 타입 변환을 통해 타입을 일치시키면 동등하다.
-5 == '5'; // -> true
+// 동등 비교, 결과를 예측하기 어렵다.
+ '0' = ''; // -> false
+0 == ''; // -> true
+0 == '0'; // -> true
+false == 'false'; // -> false
+false == '0';     // -> true
+false == null;    // -> false
+false == undefined// -> false
