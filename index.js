9d1557203e6ae411cf0502e41a2a509f6f175017
fbghk <im706374@gmail.com>
2024-09-02 04:14:51 +0900

    prac | 예제 07-22

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
-var x = 2;
+var x = 2, result;
 
 // 2 % 2는 0이고 0은 false로 암묵적 타입 변환된다.
-var result = x % 2 ? '홀수' : '짝수';
+if (x % 2) result = '홀수';
+else       result = '짝수';
 
-console.log(result); //짝수
+console.log(result); // 짝수
