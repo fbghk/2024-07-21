5d5c8ebf67345114e195056e3e9952a5815cfd78
fbghk <im706374@gmail.com>
2024-09-02 04:35:30 +0900

    prac | 예제 07-23

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
-var x = 2, result;
+var x = 10;
 
-// 2 % 2는 0이고 0은 false로 암묵적 타입 변환된다.
-if (x % 2) result = '홀수';
-else       result = '짝수';
-
-console.log(result); // 짝수
+// if ...else 문은 표현식이 아닌 문이다. 따라서 값처럼 사용할 수 없다.
+var result = if (x % 2) { result = '홀수';} else {result = '짝수'; };
+//SyntaxError: Unexpected token if
