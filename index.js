633d0b59b852ec91a5d9f3ecf4689724c4ea53c5
fbghk <im706374@gmail.com>
2024-09-02 04:36:48 +0900

    prac | 예제 07-24

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var x = 10;
 
-// if ...else 문은 표현식이 아닌 문이다. 따라서 값처럼 사용할 수 없다.
-var result = if (x % 2) { result = '홀수';} else {result = '짝수'; };
-//SyntaxError: Unexpected token if
+// 삼항 조건 연산자 표현식은 표현식인 문이다. 따라서 값처럼 사용할 수 없다.
+var result = x % 2 ? '홀수' : '짝수';
+
+console.log(result); //짝수
