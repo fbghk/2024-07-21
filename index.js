164b693ef272013a785b4750fa2bf541e723163a
fbghk <im706374@gmail.com>
2024-09-02 04:12:28 +0900

    prac | 예제 07-21

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
-// 대소 관계 비교
-5 > 0;      // -> true
-5 > 5;      // -> false
-5 >= 5;     // -> true
-5 <= 5;     // -> true
+var x = 2;
+
+// 2 % 2는 0이고 0은 false로 암묵적 타입 변환된다.
+var result = x % 2 ? '홀수' : '짝수';
+
+console.log(result); //짝수
