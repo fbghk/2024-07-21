fbac6728bcd5af5f5c349afa0bb1a1b5aa38a239
fbghk <im706374@gmail.com>
2024-09-18 02:55:32 +0900

    prac | 예제 08-02

--- index.js
+++ index.js
@@ -1,15 +1,27 @@
-// 블록문
-{
-  var foo = 10;
+var num  = 2;
+var kind;
+
+// if 문
+if (num > 0)  {
+  kind = '양수'; // 음수를 구별할 수 없다
 }
 
-// 제어문
-var x = 1;
-if (x < 10) {
-  x++;
-}
+console.log(kind); // 양수
 
-// 함수 선언문
-function sum(a, b) {
-  return a + b;
+// if...else 문
+if (num > 0) {
+  kind = '양수';
+} else {
+  kind = '음수'; // 0은 음수가 아니다.
 }
+console.log(kind) ; // 양수
+
+// if...else if 문
+if (num > 0) {
+  kind = '양수';
+} else if (num < 0) {
+  kind = '음수';
+} else {
+  kind = '영';
+}
+console.log(kind); // 양수
