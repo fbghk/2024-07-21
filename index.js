f72b172007c9b858f8954cc3257cb1367822e719
fbghk <im706374@gmail.com>
2024-09-18 18:44:45 +0900

    prac | 예제 08-20

--- index.js
+++ index.js
@@ -1,2 +1,7 @@
-// foo라는 레이블 식별자가 붙은 레이블 문
- foo: console.log('foo');
+// foo라는 식별자가 붙은 레이블 블록문
+ foo: {
+  console.log(1);
+  break foo; // foo 레이블 블록문을 탈출한다.
+  console.log(2);
+}
+console.log('Done!');
