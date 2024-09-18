c89361703c521b46a0d4272b8df8b7397b9b35ae
fbghk <im706374@gmail.com>
2024-09-18 10:59:21 +0900

    prac | 예제 08-04

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
-var num = 2;
-var kind;
+// x가 짝수이면 result 변수에 문자열 '짝수'를 할당하고, 홀수이면 문자열 '홀수'를 할당한다.
+var x = 2;
+var result;
 
-if (num > 0)       kind = '양수';
-else if (num < 0)  kind = '음수';
-else               kind = '영';
+if (x % 2) { // 2 % 2는 0이다. 이때 0은 false로 암묵적 강제 변환된다.
+  result = '홀수';
+} else {
+  result = '짝수';
+}
 
-console.log(kind); // 양수
+console.log(result); // 짝수
