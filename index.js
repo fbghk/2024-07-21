fd77e17e6d6be309b51b8fb1d464106ef806f87b
fbghk <im706374@gmail.com>
2024-09-18 21:14:27 +0900

    prac | 예제 08-23

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 var string = 'Hello World.';
 var search = 'l';
-var index;
+var count = 0 ;
 
-//문자열은 유사 배열이므로 for 문으로 순회할 수 있다.
+// 문자열은 유사 배열이므로 for 문으로 순회할 수 있다.
 for (var i = 0; i < string.length; i++) {
-  //문자열의 개별 문자가 '1'이면
-  if (string[i] === search) {
-    index = i;
-    break; // 반복문을 탈출한다.
-  }
+  // 'l'이 아니면 현 지점에서 실행을 중단하고 반복문의 증감식으로 이동한다.
+  if (string[i] !== search) continue;
+  count++; // continue 문이 실행되면 이 문은 실행되지 않는다.
 }
 
-console.log(index); //2
-//참고로 String.prototype.indexOf 매서드를 사용해도 같은 동작을 한다.
-console.log(string.indexOf(search)); // 2
+console.log(count); // 3
+
+//참고로 String.prototype.match 매서드를 사용해도 같은 동작을 한다.
+const regexp = new RegExp(search, 'g');
+console.log(string.match(regexp).length); // 3
