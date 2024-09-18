346a62c0689ef4042e1aa2bef9063d7c2345aee6
fbghk <im706374@gmail.com>
2024-09-18 18:53:45 +0900

    prac | 예제 08-22

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
-// outer라는 식별자가 붙은 레이블 for 문
-outer: for (var i = 0; i < 3; i++){
-  for (var j = 0; j < 3; j++){
-    // i + j === 3이면 outer라는 식별자가 붙은 레이블 for 문을 탈출한다.
-    if (i + j === 3) break outer;
-    console.log(`inner [${i}, ${j}]`);
+var string = 'Hello World.';
+var search = '1';
+var index;
+
+//문자열은 유사 배열이므로 for 문으로 순회할 수 있다.
+for (var i = 0; i < string.length; i++) {
+  //문자열의 개별 문자가 '1'이면
+  if (string[i] === search) {
+    index = i;
+    break; // 반복문을 탈출한다.
   }
 }
 
-console.log('Done!');
+console.log(index); //2
