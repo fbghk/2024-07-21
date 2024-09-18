a708edb680236f188191ce6e4c52d3eca0670760
fbghk <im706374@gmail.com>
2024-09-18 18:25:29 +0900

    prac | 예제 08-14

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
-for (var i = 1; i <= 6; i++) {
-  for (var j = 1; j <= 6; j++) {
-    if (i + j === 6) console.log(`[${i}, ${j}]`);
-  }
+var count = 0;
+
+// count가 3보다 작을 때까지 코드 블록을 계속 반복 실행한다.
+while (count < 3) {
+  console.log(count); // 0 1 2
+  count++;
 }
