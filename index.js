040c4bcc5b78320ecb6925f61c61f0209d64d94e
fbghk <im706374@gmail.com>
2024-09-18 11:47:33 +0900

    prac | 예제 08-09

--- index.js
+++ index.js
@@ -1,33 +1,23 @@
-// 월을 영어로 변환한다. (11 -> 'November')
-var month = 11;
-var monthName;
+var year = 2000; // 2000년은 윤년으로 2월이 29일이다.
+var month = 2;
+var days= 0;
 
 switch (month) {
-  case 1: monthName = 'January';
+  case 1: case 3: case 5: case 7: case 8: case 10: case 12:
+    days = 31;
     break;
-  case 2: monthName = 'February';
+  case 4: case 6: case 9: case 11:
+    days = 30;
     break;
-  case 3: monthName = 'March';
+  case 2: 
+    // 윤년 계산 알고리즘
+    // 1. 연도가 4로 나누어 떨어지는 해(2000, 2004, 2008, 2012, 2016, 2020...)는 윤년이다.
+    // 2. 연도가 4로 나누어 떨어지더라도 연도가 100으로 나누어 떨어지는 해 (2000, 2100, 2200...)는 평년이다.
+    // 3. 연도가 400으로 나누어 떨어지는 해(2000, 2400, 2800...)는 윤년이다.
+    days = ((year % 4 === 0 && year % 100 !== 0 ) || (year % 400 === 0)) ? 29 : 28;
     break;
-  case 4: monthName = 'April';
-    break;
-  case 5: monthName = 'May';
-    break;
-  case 6: monthName = 'June';
-    break;
-  case 7: monthName = 'July';
-    break;
-  case 8: monthName = 'August';
-    break;
-  case 9: monthName = 'September';
-    break;
-  case 10: monthName = 'October';
-    break;
-  case 11: monthName = 'November';
-    break;
-  case 12: monthName = 'December';
-    break;
-  default: monthName = 'Invalid month';
+  default: 
+    console.log('Invalid month');
 }
 
 console.log(monthName); // November
