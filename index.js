430a29406152cfc2094dec53df86c042f85259a2
fbghk <im706374@gmail.com>
2024-09-18 21:34:23 +0900

    prac | 예제 09-01

--- index.js
+++ index.js
@@ -1,21 +1,9 @@
-// continue 문을 사용하지 않으면 if 문 내에 코드를 작성해야 한다.
-for (var i = 0; i < string.length; i++){
-  // 'l'이면 카운트를 증가시킨다.
-  if (string[i] === search){
-    count++;
-    //code
-    //code
-    //code
-  }
-}
+var x = 10;
 
-// continue 문을 사용하지 않으면 if 문 내에 코드를 작성할 수 없다.
-for (var i = 0; i < string.length; i++){
-  // 'l'이 아니면 카운트를 증가시키지 않는다.
-  if (string[i] === search) continue;
-  
-    count++;
-    //code
-    //code
-    //code
-}
+// 명시적 타입 변환
+// 숫자를 문자열로 타입 캐스팅한다.
+var str = x.toString();
+console.log(typeof str, str); // string 10
+
+// x 변수의 값이 변경된 것은 아니다.
+console.log(typeof x, x); // number 10
