169cc1c23e81adae32e99bf12f0c4b0c0d44878f
fbghk <im706374@gmail.com>
2024-09-18 18:30:15 +0900

    prac | 예제 08-17

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 var count = 0;
 
-// 무한루프
-while (true) {
-  console.log(count); 
+// count가 3보다 작을 때까지 코드 블록을 계속 반복 실행한다.
+do {
+  console.log(count); // 0 1 2
   count++;
-  // count가 3이면 코드 블록을 탈출한다.
-  if (count === 3) break;
-} // 0 1 2
+} while (count < 3);
