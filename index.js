dcda17fdac2729b6949582d66efae96ac029f09c
fbghk <im706374@gmail.com>
2024-09-18 18:26:27 +0900

    prac | 예제 08-15

--- index.js
+++ index.js
@@ -1,7 +1,2 @@
-var count = 0;
-
-// count가 3보다 작을 때까지 코드 블록을 계속 반복 실행한다.
-while (count < 3) {
-  console.log(count); // 0 1 2
-  count++;
-}
+// 무한루프 조건식의 평가 결과가 언제나 참이면 무한루프가 된다.
+while (true) { ... }
