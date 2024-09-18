c41306d59e778ddc9140d77a701f095d104e218f
fbghk <im706374@gmail.com>
2024-09-18 11:30:19 +0900

    prac | 예제 08-07

--- index.js
+++ index.js
@@ -1,5 +1,21 @@
-var num = 2;
+// 월을 영어로 변환한다. (11 -> 'November')
+var month = 11;
+var monthName;
 
-// 0은 false로 취급된다.
-var kind = num ? (num > 0 ? '양수' : '음수'): '영';
-console.log(kind); // 양수
+switch (month) {
+  case 1: monthName = 'January';
+  case 2: monthName = 'February';
+  case 3: monthName = 'March';
+  case 4: monthName = 'April';
+  case 5: monthName = 'May';
+  case 6: monthName = 'June';
+  case 7: monthName = 'July';
+  case 8: monthName = 'August';
+  case 9: monthName = 'September';
+  case 10: monthName = 'October';
+  case 11: monthName = 'November';
+  case 12: monthName = 'December';
+  default: monthName = 'Invalid month';
+}
+
+console.log(monthName); // Invalid month
