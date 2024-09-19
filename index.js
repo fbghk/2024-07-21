35793d734de27ae349d7c192af2dab5e1f303f4f
fbghk <im706374@gmail.com>
2024-09-19 12:51:29 +0900

    prac | 예제 09-12

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
-if ('')    console.log('1');
-if (true)  console.log('2');
-if (0)     console.log('3');
-if ('str') console.log('4');
-if (null)  console.log('5');
-
+// 아래의 조건문은 모두 코드 블록을 실행한다.
+if (!false)      console.log(false + ' is falsy value');
+if (!undefined)  console.log(undefined + ' is falsy value');
+if (!null)       console.log(null + ' is falsy value');
+if (!0)          console.log(0 + ' is falsy value');
+if (!NaN)        console.log(NaN + ' is falsy value');
+if (!'')         console.log('' + ' is falsy value');
 // 2 4
