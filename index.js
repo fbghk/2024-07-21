b26568e233da447f387bc483eaeda2cc09350e1e
fbghk <im706374@gmail.com>
2024-09-19 17:36:51 +0900

    prac | 예제 09-19

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-'cat' || 'dog' // -> 'cat'
+// 논리합 (||) 연산자
+'Cat' || 'Dog' // -> 'Cat'
+false || 'Dog' // -> 'Dog'
+'Cat' || false // -> 'Cat'
+
+// 논리곱 (&&) 연산자
+'Cat' && 'Dog' // -> 'Dog'
+false && 'Dog' // -> false
+'Cat' && false // -> false
