581d6c97969403403b21656ca9b34c9f6fb6718a
fbghk <im706374@gmail.com>
2024-09-23 01:30:19 +0900

    prac | 예제 09-27

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 var elem = null;
 
-// elem이 null 또는 undefined를 반환하고, 그렇지 않으면 우항의 프로퍼티 참조를 이어간다.
-var value = elem?.value;
-console.log(value); //undefined
+// elem이 Falsy 값이면 elem으로 평가되고, elem이 Truthy 값이면 elem.value로 평가된다.
+var value = elem && elem.value;
+console.log(value); // null
