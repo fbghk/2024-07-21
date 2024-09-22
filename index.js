26dcdf3148a323a022fcdd64ddff8d124dadb434
fbghk <im706374@gmail.com>
2024-09-23 01:39:34 +0900

    prac | 예제 09-28

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
-var elem = null;
+var str = '';
 
-// elem이 Falsy 값이면 elem으로 평가되고, elem이 Truthy 값이면 elem.value로 평가된다.
-var value = elem && elem.value;
-console.log(value); // null
+// 문자열의 길이 (length)를 참조한다.
+var length = str && str.length;
+
+// 문자열의 길이 (length)를 참조하지 못한다.
+console.log(length); // ''
