9b99945443702dc0b1200639dda37129fd45b487
fbghk <im706374@gmail.com>
2024-09-23 01:26:49 +0900

    prac | 예제 09-26

--- index.js
+++ index.js
@@ -1,16 +1,5 @@
-// 단축 평가를 사용한 매개변수의 기본값 설정
-function getStringLength(str) {
-  str = str || '';
-  return str.length;
-}
+var elem = null;
 
-getStringLength();      // -> 0
-getStringLength('hi');  // -> 2
-
-// ES6의 매개변수의 기본값 설정
-function getStringLength(str = '') {
-  return str.length;
-}
-
-getStringLength();      // -> 0
-getStringLength('hi');  // -> 2
+// elem이 null 또는 undefined를 반환하고, 그렇지 않으면 우항의 프로퍼티 참조를 이어간다.
+var value = elem?.value;
+console.log(value); //undefined
