439d5e6a51e717a079d069f5f8975ccce16e5f10
fbghk <im706374@gmail.com>
2024-09-23 23:24:39 +0900

    prac | 예제 09-29

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 var str = '';
 
-// 문자열의 길이 (length)를 참조한다.
-var length = str && str.length;
-
-// 문자열의 길이 (length)를 참조하지 못한다.
-console.log(length); // ''
+// 문자열의 길이(length)를 참조한다. 이때 좌향 피연산자가 false로 평가되는 Falsy 값이라도
+//null 또는 undefined가 아니면 우향의 프로퍼티 참조를 이어간다.
+var length = str?.length;
+console.log(length); // 0
