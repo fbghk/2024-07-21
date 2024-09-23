ed186fc202b079e6e5f84b8c902777f58224fdc1
fbghk <im706374@gmail.com>
2024-09-23 23:37:44 +0900

    prac | 예제 09-31

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-// 좌항의 피연산자가 null 또는 undefined이면 우항의 피연산자를 반환하고,
-// 그렇지 않으면 좌항의 피연산자를 반환한다.
-var foo = null ?? 'default string';
+// Falsy 값인 0 이나 ''도 기본값으로서 유효하다면 예기치 않은 동작이 발생할 수 있다.
+var foo = '' || 'default string';
 console.log(foo); // "default string"
