916bd7482dfbb1e2007b1700a97160604515c3c7
fbghk <im706374@gmail.com>
2024-09-25 22:40:36 +0900

    prac | 예제 10-05

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
 var person = { 
-  firstName : 'Ung-mo', // 식별자 네이밍 규칙을 준수하는 프로퍼티 키
-  'last-name' : 'Lee'   // 식별자 네이밍 규칙을 준수하지 않는 프로퍼티 키
+  firstName : 'Ung-mo', 
+  last-name : 'Lee'   // SyntaxError: Unexpected token '-'
 }; 
-
-console.log(person); // {firstName: "Ung-mo", last-name: "Lee"}
