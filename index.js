e1a50c3a5ecb854d1e4921efc849fc3892205c7a
fbghk <im706374@gmail.com>
2024-09-26 23:16:43 +0900

    prac | 예제 10-07

--- index.js
+++ index.js
@@ -1,9 +1,5 @@
-var obj = {}; 
-var key = 'hello';
+var foo = {
+  '' : '' // 빈 문자열도 프로퍼티 키로 사용할 수 있다.
+}; 
 
-// ES5: 프로퍼티 키 동적 생성
-obj[key] = 'world';
-// ES6: 계산된 프로퍼티 이름
-// var obj = { [key]: 'world' };
-
-console.log(obj); // {hello: "world"}
+console.log(foo); // {"" : ""}
