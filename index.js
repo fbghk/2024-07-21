95d8606f88b054ad9559e4f42e7eae505aaf5e2f
fbghk <im706374@gmail.com>
2024-09-29 22:08:48 +0900

    prac | 예제 10-11

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
-var foo = {
-  name : 'Lee',
-  name : 'Kim',
-}; 
+var circle = {
+  radius: 5, // <- 프로퍼티
 
-console.log(foo); // {name : "Kim"}
+  // 원의 지름
+  getDiameter: function () { // <- 메서드
+    return 2 * this.radius; // this는 circle을 가리킨다.
+  }
+};
+
+console.log(circle.getDiameter()); // 10
