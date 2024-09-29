e23aba695737bf71b9e6dc1f6ff421376c999b8d
fbghk <im706374@gmail.com>
2024-09-29 22:24:30 +0900

    prac | 예제 10-13

--- index.js
+++ index.js
@@ -2,8 +2,4 @@ var person = {
   name: 'Lee'
 };
 
-// 마침표 표기법에 의한 프로퍼티 접근
-console.log(person.name); // Lee
-
-// 대괄호 표기법에 의한 프로퍼티 접근
-console.log(person['name']); // Lee
+console.log(person[name]); // ReferenceError: name is not defined
