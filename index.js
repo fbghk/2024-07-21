c3b653d888239b3fe1f484e47adadb36beee6dd5
fbghk <im706374@gmail.com>
2024-10-04 22:17:40 +0900

    prac | 예제 10-16

--- index.js
+++ index.js
@@ -1,16 +1,8 @@
 var person = {
-  'last-name': 'Lee',
-  1: 10
+  name: 'Lee',
 };
 
-person.'last-name'; // -> SyntaxError: Unexpected string
-person.last-name;   // -> 브라우저 환경: NaN
-                    // -> Node.js 환경: ReferenceError: name is not defined
-person.[last-name];   // -> ReferenceError: name is not defined
-person.['last-name'];   // -> Lee
+// person 객체에 name 프로퍼티가 존재하므로 name 프로퍼티의 값이 갱신된다.
+person.name = 'Kim'; 
 
-// 프로퍼티 키가 숫자로 이뤄진 문자열인 경우 따옴표를 생략할 수 있다.
-person.1; // SyntaxError: Unexpected number
-person.'1';  // -> SyntaxError: Unexpected string
-person[1];  // -> 10 : person[1] -> person['1']
-person['1'];  // -> 10
+console.log(person); // {name: "Kim"}
