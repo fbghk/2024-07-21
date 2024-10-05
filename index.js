337d53e5f0363e818b473a1dfd8f8e725588c7a8
fbghk <im706374@gmail.com>
2024-10-05 16:02:12 +0900

    prac | 예제 10-21

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
-// ES6 
-var x = 1, y = 2;
+// ES5
+var prefix = 'prop';
+var i = 0;
 
-var obj = { x, y };
+var obj = {};
 
-console.log(obj); // {x: 1, y: 2}
+//계산된 프로퍼티 이름으로 프로퍼티 키 동적 생성
+obj[prefix + '-' + ++i] = i;
+obj[prefix + '-' + ++i] = i;
+obj[prefix + '-' + ++i] = i;
+
+console.log(obj); // {prop-1: 1, prop-2: 2, prop-3: 3}
