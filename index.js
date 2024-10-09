f510106e4cdf4b6114cd298df8b8b2d91a09b8cc
fbghk <im706374@gmail.com>
2024-10-09 23:56:57 +0900

    prac | 예제 11-05

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 var str = 'string';       
 
 // 문자열은 유사 배열이므로 배열과 유사하게 인덱스를 사용해 각 문자에 접근할 수 있다.
-console.log(str[0]); // s
+// 하지만 문자열은 원시 값이므로 변경할 수 없다. 이때 에러가 발생하지 않는다.
+str[0] = '5';
 
-//원시 값인 문자열이 객체처럼 동작한다.
-console.log(str.length); // 6
-console.log(str.toUpperCase()); // STRING
+console.log(str); // string
