08b9681eef7efd2190fdd8f2b1b9174695fe3ab8
fbghk <im706374@gmail.com>
2024-10-13 16:24:45 +0900

    prac | 예제 11-07

--- index.js
+++ index.js
@@ -1,10 +1,7 @@
 var score = 80;
+
+//copy 변수에는 score 변수의 값 80이 복사되어 할당된다.
 var copy = score;
-
-console.log(score); // 80
-console.log(copy); // 80
-
-score = 100;
-
-console.log(score); // 100
-console.log(copy); // ?
+ 
+console.log(score, copy); // 80  80
+console.log(score === copy); // true
