307c9fdd48feeb2b9917338637caa3b30dbb8547
fbghk <im706374@gmail.com>
2024-11-17 23:56:52 +0900

    prac | 예제 11-13

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
-// 혈당이 이뤄지는 시점에 객체 리터럴이 해석되고, 그 결과 객체가 생성된다.
 var person = {
   name: 'Lee'
 };
 
-// person 변수에 저장되어 있는 참조 값으로 실제 객체에 접근한다.
-console.log(person); // {name: "Lee"}
+// 프로퍼티 값 갱신
+person.name = 'Kim';
+
+//프로퍼티 동적 생성
+person.address = "Seoul";
+
+console.log(person); // {name: "Kim", address: "Seoul"}
