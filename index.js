299d42d2e3dc28dac049538eec67e6369f78f86b
fbghk <im706374@gmail.com>
2024-11-23 23:35:22 +0900

    prac | 예제 11-14

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
-var person = {
-  name: 'Lee'
-};
+const o = { x: { y: 1 }};
 
-// 프로퍼티 값 갱신
-person.name = 'Kim';
+//얕은 복사
+const c1 = { ...o };  // 35장 " 스프레드 문법" 참고
+console.log(c1 === o);// false
+console.log(c1.x === o.x);// true
 
-//프로퍼티 동적 생성
-person.address = "Seoul";
-
-console.log(person); // {name: "Kim", address: "Seoul"}
+// lodash의 cloneDeep을 사용한 깊은 복사
+// "npm install lodash"로 lodash를 설치한 후, Node.js 환경에서 실행
+const _ = require('lodash');
+//깊은 복사
+const c2 = _.cloneDeep(o);
+console.log(c2 === o);// false
+console.log(c2.x === o.x);// false
